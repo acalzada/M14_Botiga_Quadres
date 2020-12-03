c119662dca0088cd5d5453130cd70fa5f2c61843
merdaalcanto <merdaalcanto@hotmail.com>
2020-12-03 19:55:37 +0100

    initial commit.

---
+++ src/main/java/com/M14WhiteCollar/M14BotigaQuadresApplication.java
@@ -0,0 +1,13 @@
+package com.M14WhiteCollar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M14BotigaQuadresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M14BotigaQuadresApplication.class, args);
+	}
+
+}
