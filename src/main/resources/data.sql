dbf916646675d9544fc18e36357fe452ded9a9c6
merdaalcanto <merdaalcanto@hotmail.com>
2020-12-17 20:02:44 +0100

    changed H2 embbeded database for MySQL localhost server. Added dependencies into POM.XML and specified the database name into the data.sql data source file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO STORES (NAME, MAX_CAPACITY) VALUES ('Collars BCN', '100');
-INSERT INTO STORES (NAME, MAX_CAPACITY) VALUES ('Collars Bilbo', '200');
-INSERT INTO STORES (NAME, MAX_CAPACITY) VALUES ('Collares Caninos divinos', '300');
+INSERT INTO whitecollar_db.STORES (NAME, MAX_CAPACITY) VALUES ('Collars BCN', '100');
+INSERT INTO whitecollar_db.STORES (NAME, MAX_CAPACITY) VALUES ('Collars Bilbo', '200');
+INSERT INTO whitecollar_db.STORES (NAME, MAX_CAPACITY) VALUES ('Collares Caninos divinos', '300');
 
-INSERT INTO FRAMES (author, name, price, store_entry_date, store_id ) VALUES ('ACD', 'La Gioconda', 100, '20201113', 2);
-INSERT INTO FRAMES (author, name, price, store_entry_date, store_id) VALUES ('Pepe', 'La Maja vestida', 300, '20201113', 3);
-INSERT INTO FRAMES (author, name, price, store_entry_date, store_id) VALUES ('MAria', 'El Querjhica', 400, '20191113', 2);
-INSERT INTO FRAMES (author, name, price, store_entry_date, store_id) VALUES ('Laura', 'El Gritoooor', 500, '20181113', 1);
+INSERT INTO whitecollar_db.FRAMES (author, name, price, store_entry_date, store_id ) VALUES ('ACD', 'La Gioconda', 100, '20201113', 2);
+INSERT INTO whitecollar_db.FRAMES (author, name, price, store_entry_date, store_id) VALUES ('Pepe', 'La Maja vestida', 300, '20201113', 3);
+INSERT INTO whitecollar_db.FRAMES (author, name, price, store_entry_date, store_id) VALUES ('MAria', 'El Querjhica', 400, '20191113', 2);
+INSERT INTO whitecollar_db.FRAMES (author, name, price, store_entry_date, store_id) VALUES ('Laura', 'El Gritoooor', 500, '20181113', 1);
